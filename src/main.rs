f4d819bee5784114f62471fe748ce08700b72874
unknown <5dpixel56@gmail.com>
2025-04-17 22:04:57 +0100

    Update ISA docs

--- src/main.rs
+++ src/main.rs
@@ -10,24 +10,27 @@ fn main() {
     let mut vcpu = cpu::MicroCVMCpu::empty();
     let mut vdisk = disk::MicroCVMDisk::empty();
 
-    vcpu.memory[0] = cpu::OpcodeType::Mov as u8;
-    vcpu.memory[1] = cpu::Register::V0 as u8;
-    vcpu.memory[2] = 255;
-    vcpu.memory[3] = cpu::OpcodeType::Mov as u8;
-    vcpu.memory[4] = cpu::Register::V1 as u8;
-    vcpu.memory[5] = 0;
-    vcpu.memory[6] = cpu::OpcodeType::Mov as u8;
-    vcpu.memory[7] = cpu::Register::V2 as u8;
-    vcpu.memory[8] = 255;
-    vcpu.memory[9] = cpu::OpcodeType::Call as u8;
-    vcpu.memory[10] = cpu::FunctionCall::FillRect as u8;
-    vcpu.memory[11] = cpu::OpcodeType::Hlt as u8;
-    // match vcpu.read_memory_from_file("../../tests/mov.bin") {
-    //     Ok(_) => {}
-    //     Err(e) => {
-    //         eprintln!("error reading binary: {}", e);
-    //     }
+    // vcpu.memory[0] = cpu::OpcodeType::Mov as u8;
+    // vcpu.memory[1] = cpu::Register::V0 as u8;
+    // vcpu.memory[2] = 255;
+    // vcpu.memory[3] = cpu::OpcodeType::Mov as u8;
+    // vcpu.memory[4] = cpu::Register::V1 as u8;
+    // vcpu.memory[5] = 0;
+    // vcpu.memory[6] = cpu::OpcodeType::Mov as u8;
+    // vcpu.memory[7] = cpu::Register::V2 as u8;
+    // vcpu.memory[8] = 255;
+    // vcpu.memory[9] = cpu::OpcodeType::Call as u8;
+    // vcpu.memory[10] = cpu::FunctionCall::FillRect as u8;
+    // vcpu.memory[11] = cpu::OpcodeType::Hlt as u8;
+    // for i in 0..11 {
+    //     println!("{:x}", vcpu.memory[i]);
     // }
+    match vcpu.read_memory_from_file("../../tests/fill.bin") {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("error reading binary: {}", e);
+        }
+    }
 
     loop {
         let opcode: u8 = vcpu.memory[vcpu.pc as usize];
