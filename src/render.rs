c025b006584376d75e89429c8aa9d8e10f28e211
CordlessCoder <roman@vm.net.ua>
2025-04-17 16:29:25 +0100

    Use iterators for framebuffer

--- src/render.rs
+++ src/render.rs
@@ -26,7 +26,7 @@ impl ApplicationHandler for App {
         let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
         self.window = Some(window.clone());
 
-        let surface_texture = SurfaceTexture::new(self.width / 2, self.height / 2, window.clone());
+        let surface_texture = SurfaceTexture::new(self.width / 2, self.height / 2, window);
         let pixels = Pixels::new(self.width / 2, self.height / 2, surface_texture).unwrap();
 
         self.pixels = Some(pixels);
@@ -54,32 +54,29 @@ impl ApplicationHandler for App {
 
 impl App {
     fn render(&mut self) {
-        if let Some(pixels) = self.pixels.as_mut() {
-            let frame = pixels.frame_mut();
+        let Some(pixels) = self.pixels.as_mut() else {
+            return;
+        };
+        let frame = pixels.frame_mut();
 
-            if self.video_memory.len() < frame.len() {
-                eprintln!(
-                    "Error: Video memory size does not match framebuffer size. Frame size: {}, Video memory size: {}",
-                    frame.len(),
-                    self.video_memory.len()
-                );
-                return;
-            }
-
-            let mut byte_index = 0;
-            for color in &self.video_memory {
-                if byte_index + 3 >= frame.len() {
-                    break;
-                }
-                frame[byte_index] = color.r;
-                frame[byte_index + 1] = color.g;
-                frame[byte_index + 2] = color.b;
-                frame[byte_index + 3] = color.a;
-                byte_index += 4;
-            }
-
-            pixels.render().unwrap();
+        if self.video_memory.len() < frame.len() {
+            eprintln!(
+                "Error: Video memory size does not match framebuffer size. Frame size: {}, Video memory size: {}",
+                frame.len(),
+                self.video_memory.len()
+            );
+            return;
         }
+
+        for (chunk, color) in frame.chunks_exact_mut(4).zip(&self.video_memory) {
+            let [r, g, b, a] = chunk else { unreachable!() };
+            *r = color.r;
+            *g = color.g;
+            *b = color.b;
+            *a = color.a;
+        }
+
+        pixels.render().unwrap();
     }
 
     pub fn new(width: u32, height: u32, video_memory: Vec<super::types::Color>) -> Self {
